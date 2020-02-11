48a4bc81557fb6bc3a4c659c36d0b3715b88db1d
Bjarne Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2020-02-11 07:26:10 +0100

    Added grid layout and scaffolded components

--- README.md
+++ README.md
@@ -9,7 +9,11 @@ Small webapp to show planets and their details
 - [x] Setup lazy loaded feature module with Page/components architecture and routing
 - [x] Master detail routing - Master should always be beneath, and detail route should shown above, with back buttom to existing master view
 - [x] Make simple responsive effect
+- [x] Use css grid for layout and flexbox for one dimensional content
+- [ ] Make header component
+- [ ] Make list component
 - [ ] Fetch planets and single planet api
 - [ ] Make design for master list view matching detail view
 - [ ] Make detail view matching the design
 - [ ] Optimize images ?
+- [ ] Make router transition animation ?
--- src/app/features/planets/planets.module.ts
+++ src/app/features/planets/planets.module.ts
@@ -5,9 +5,12 @@ import { RouterModule } from '@angular/router';
 import { PlanetsDetailComponent } from './pages/planets-detail/planets-detail.component';
 import { PlanetsMasterComponent } from './pages/planets-master/planets-master.component';
 import { PlanetsRoutingModule } from './planets-routing.module';
+import { HeaderComponent } from './components/header/header.component';
+import { ListComponent } from './components/list/list.component';
+import { DetailComponent } from './components/detail/detail.component';
 
 @NgModule({
-  declarations: [PlanetsMasterComponent, PlanetsDetailComponent],
+  declarations: [PlanetsMasterComponent, PlanetsDetailComponent, HeaderComponent, ListComponent, DetailComponent],
   imports: [CommonModule, PlanetsRoutingModule, RouterModule]
 })
 export class PlanetsModule {}
