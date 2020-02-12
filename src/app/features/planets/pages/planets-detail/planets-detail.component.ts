408b9afe363059d4cd8402ce0aa964d986e0e161
Bjarne Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2020-02-12 07:07:18 +0100

    Add detail design

--- src/app/features/planets/pages/planets-detail/planets-detail.component.ts
+++ src/app/features/planets/pages/planets-detail/planets-detail.component.ts
@@ -13,6 +13,23 @@ import { Planet } from 'src/app/core/models/planet';
 export class PlanetsDetailComponent implements OnInit {
   planet$: Observable<Planet>;
 
+  // planetPlaceholder$ = of({
+  //   name: 'Yavin IV',
+  //   rotation_period: '24',
+  //   orbital_period: '4818',
+  //   diameter: '10200',
+  //   climate: 'temperate, tropical',
+  //   gravity: '1 standard',
+  //   terrain: 'jungle, rainforests',
+  //   surface_water: '8',
+  //   population: '1000',
+  //   residents: [],
+  //   films: ['https://swapi.co/api/films/1/'],
+  //   created: '2014-12-10T11:37:19.144000Z',
+  //   edited: '2014-12-20T20:58:18.421000Z',
+  //   url: 'https://swapi.co/api/planets/3/'
+  // }).pipe(map(res => new Planet(res)));
+
   constructor(
     private route: ActivatedRoute,
     private swapiService: SwapiService
