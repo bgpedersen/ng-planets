75c770f16268bea5330d289ad49c7fffff3d0684
Bjarne Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2020-02-12 19:10:59 +0100

    Fix overflow and added fake apis for easier testing to avoid cors errors

--- src/app/features/planets/pages/planets-detail/planets-detail.component.ts
+++ src/app/features/planets/pages/planets-detail/planets-detail.component.ts
@@ -12,23 +12,7 @@ import { Planet } from 'src/app/core/models/planet';
 })
 export class PlanetsDetailComponent implements OnInit {
   planet$: Observable<Planet>;
-
-  // planetPlaceholder$ = of({
-  //   name: 'Yavin IV',
-  //   rotation_period: '24',
-  //   orbital_period: '4818',
-  //   diameter: '10200',
-  //   climate: 'temperate, tropical',
-  //   gravity: '1 standard',
-  //   terrain: 'jungle, rainforests',
-  //   surface_water: '8',
-  //   population: '1000',
-  //   residents: [],
-  //   films: ['https://swapi.co/api/films/1/'],
-  //   created: '2014-12-10T11:37:19.144000Z',
-  //   edited: '2014-12-20T20:58:18.421000Z',
-  //   url: 'https://swapi.co/api/planets/3/'
-  // }).pipe(map(res => new Planet(res)));
+  planetFake$: Observable<Planet>;
 
   constructor(
     private route: ActivatedRoute,
@@ -41,6 +25,10 @@ export class PlanetsDetailComponent implements OnInit {
 
     this.planet$ = this.swapiService
       .getPlanet(id)
+      .pipe(map(res => new Planet(res)));
+
+    this.planetFake$ = this.swapiService
+      .getFakePlanet()
       .pipe(map(res => new Planet(res)));
   }
 }
